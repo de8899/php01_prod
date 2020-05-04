9501f048615020bafca0afd3e696d1e6f5a29fb0
de8899 <de8899@zoho.com>
2020-05-04 11:47:51 -0700

    first upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles_v1.0.css">
+</head>
+<body>
+
+<h1>PHP site 1</h1>
+
+<?php
+echo "<p> <font color=blue size='5pt'>php block start/end </font> </p>";
+?>
+
+<p>version 1.1</p>
+</body>
+</html> 
